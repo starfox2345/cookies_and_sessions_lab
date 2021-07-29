149b2251d180e2ade5ca1fa9c9960e4955bcb7b8
yatsun <yatsunlam25@gmail.com>
2021-07-29 01:10:15 -0400

    commit tree 15e77164a58e649f481d3f596cd96455a4757e19

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'products/index'
   post 'products/add'
+  # resources :products
+
   root 'products#index'
+  
 end
